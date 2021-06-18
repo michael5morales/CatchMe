bf766153a29415ed54a1a929e844e745fc121a6f
michael5morales <michael5morales@aol.com>
2021-06-18 02:17:41 -0400

    UI and button functionality

--- CatchMe/ContentView.swift
+++ CatchMe/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var xCoor: CGFloat = 200
+    @State private var yCoor: CGFloat = 200
+    @State private var score: Int = 0
+    @State private var gameOver: Bool = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            ZStack {
+                Text("Score: \(score)")
+                    .font(.title3)
+                    .bold()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        
+                    }) {
+                        Text("Play Again")
+                            
+                    }
+                    .padding(.trailing, 15.0)
+                    .disabled(gameOver)
+                    
+                }
+            }
+            GeometryReader { geometry in
+                Button(action: {
+                    
+                    xCoor = CGFloat.random(in: 150...(geometry.size.width - 150))
+                    
+                    yCoor = CGFloat.random(in:150...(geometry.size.height - 150))
+                    
+                    score += 1
+                    
+                }) {
+                    Text("Catch me if you can")
+                        .fontWeight(.regular)
+                        .font(.footnote)
+                        .padding()
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .frame(width: 200, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                        
+                        
+                }
+                .frame(width: /*@START_MENU_TOKEN@*/150.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/150.0/*@END_MENU_TOKEN@*/)
+                .background(Color.purple)
+                .cornerRadius(600)
+                .position(x: (xCoor), y: yCoor)
+            }
+            Spacer()
+        }
     }
 }
 
