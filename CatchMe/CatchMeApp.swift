92419fda57e99422ee73000690ad70e7482c7c05
michael5morales <michael5morales@aol.com>
2021-06-18 00:16:48 -0400

    Initial Commit

---
+++ CatchMe/CatchMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchMeApp.swift
+//  CatchMe
+//
+//  Created by Mike Morales on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CatchMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
